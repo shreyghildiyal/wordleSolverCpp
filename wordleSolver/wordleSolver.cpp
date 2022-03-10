545d29550485656df03fd3bd76d08b26ee46fe2e
Shrey <supershrey@gmail.com>
2022-03-10 13:29:32 +0530

    first commit

---
+++ wordleSolver/wordleSolver.cpp
@@ -0,0 +1,16 @@
+﻿// wordleSolver.cpp : Defines the entry point for the application.
+//
+
+#include "wordleSolver.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+
+
+
+	return 0;
+}
